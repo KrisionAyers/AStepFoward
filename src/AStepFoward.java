546adbb4132a9080754e072880fad8d0c62b67d0
KrisionAyers <ayersk@student.swosu.edu>
2015-10-12 09:20:34 -0500

    added the numbers for question one

--- src/AStepFoward.java
+++ src/AStepFoward.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class AStepFoward {	 
 	public static void main(String[] args) {
 	
@@ -7,10 +8,21 @@ public class AStepFoward {
 		
 		// Round 1
 		// Generate 2 random numbers
+		
+		int number1 = (int)(Math.random() *10);
+		
+		int number2 = (int)(Math.random() *10);
+		
 		// Ask the user to add these two numbers together
+		System.out.println
+		("What is " + number1 + " + " + number2 + "?");
+		
 		// Read in their response
+		Scanner input = new Scanner(System.in);
+		int studentAnswer = input.nextInt();
 		// Check if the answer was correct
 		//		IF correct
+		int correctAnswer = number1 + number2;
 		//			Tell them it was correct
 		//			Give them points
 		//			Make the next question harder
